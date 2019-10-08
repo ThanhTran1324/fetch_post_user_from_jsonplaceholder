0593c1ffa69362bdb6b3a48f34246bf39a477fd0
thanhtran1324 <thanhtran1324@gmail.com>
2019-10-08 10:03:47 -0400

    Updates

---
+++ precache-manifest.08b5c7f18b84d0491d6498f77748acd4.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6af7c8fc0dc29c455e7c81bb535b7f3d",
+    "url": "/fetch_post_user_from_jsonplaceholder/index.html"
+  },
+  {
+    "revision": "c416d57012f8df6da2fd",
+    "url": "/fetch_post_user_from_jsonplaceholder/static/js/2.80902c0a.chunk.js"
+  },
+  {
+    "revision": "4f7856aef4abc374b846",
+    "url": "/fetch_post_user_from_jsonplaceholder/static/js/main.fae96413.chunk.js"
+  },
+  {
+    "revision": "213a1b3d261334273ba4",
+    "url": "/fetch_post_user_from_jsonplaceholder/static/js/runtime-main.5a12d56a.js"
+  }
+]);
